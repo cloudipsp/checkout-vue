723c2ce242bde89f4639ed3e665c48a767c68d56
Михайло Голубовський <lamerumiata@gmail.com>
2017-11-13 17:40:40 +0200

    regular fields name

--- src/components/regular.vue
+++ src/components/regular.vue
@@ -13,23 +13,23 @@
         <div class="form-group">
           <label for="f-regular-every">Периодичность</label>
           <div class="row">
-            <div class="col-xs-6" :class="{'has-error': errors.has('every')}">
-              <tooltip :text="errors.first('every')" :enable="errors.has('every')" placement="bottom">
+            <div class="col-xs-6" :class="{'has-error': errors.has('f-regular-every')}">
+              <tooltip :text="errors.first('f-regular-every')" :enable="errors.has('f-regular-every')" placement="bottom">
               <input
-                name="every"
+                name="f-regular-every"
                 v-validate="'required|numeric'"
                 v-model="form.every"
                 type="tel"
                 class="form-control"
                 id="f-regular-every"
               >
-              <!--<div v-if="errors.has('every')" class="f-error">{{ errors.first('every') }}</div>-->
+              <!--<div v-if="errors.has('f-regular-every')" class="f-error">{{ errors.first('f-regular-every') }}</div>-->
               </tooltip>
             </div>
-            <div class="col-xs-6" :class="{'has-error': errors.has('period')}">
-              <tooltip :text="errors.first('period')" :enable="errors.has('period')">
+            <div class="col-xs-6" :class="{'has-error': errors.has('f-regular-period')}">
+              <tooltip :text="errors.first('f-regular-period')" :enable="errors.has('f-regular-period')">
               <select
-                name="period"
+                name="f-regular-period"
                 v-validate="'required'"
                 v-model="form.period"
                 class="form-control"
@@ -41,17 +41,17 @@
                 <option value="month">Месяц</option>
                 <option value="year">Год</option>
               </select>
-              <!--<div v-if="errors.has('period')" class="f-error">{{ errors.first('period') }}</div>-->
+              <!--<div v-if="errors.has('f-regular-period')" class="f-error">{{ errors.first('f-regular-period') }}</div>-->
               </tooltip>
             </div>
           </div>
         </div>
-        <div class="form-group" :class="{'has-error': errors.has('amount')}">
+        <div class="form-group" :class="{'has-error': errors.has('f-regular-amount')}">
           <label for="f-regular-amount">Сумма к оплате</label>
           <div class="input-group">
-            <tooltip :text="errors.first('amount')" :enable="errors.has('amount')">
+            <tooltip :text="errors.first('f-regular-amount')" :enable="errors.has('f-regular-amount')">
             <input
-              name="amount"
+              name="f-regular-amount"
               v-validate="'required|decimal:2'"
               v-model="form.amount"
               data-vv-as="Сумма к оплате"
@@ -59,16 +59,16 @@
               class="form-control"
               id="f-regular-amount"
             >
-            <!--<div v-if="errors.has('amount')" class="f-error">{{ errors.first('amount') }}</div>-->
+            <!--<div v-if="errors.has('f-regular-amount')" class="f-error">{{ errors.first('f-regular-amount') }}</div>-->
             </tooltip>
             <span class="input-group-addon">UAH</span>
           </div>
         </div>
-        <div class="form-group" :class="{'has-error': errors.has('start_time')}">
+        <div class="form-group" :class="{'has-error': errors.has('f-regular-start-time')}">
           <label for="f-regular-start-time">Начать с</label>
-          <tooltip :text="errors.first('start_time')" :enable="errors.has('start_time')">
+          <tooltip :text="errors.first('f-regular-start-time')" :enable="errors.has('f-regular-start-time')">
           <input
-            name="start_time"
+            name="f-regular-start-time"
             v-validate="'required'"
             v-model="form.start_time"
             data-vv-as="Начать с"
@@ -76,7 +76,7 @@
             class="form-control"
             id="f-regular-start-time"
           >
-          <!--<div v-if="errors.has('start_time')" class="f-error">{{ errors.first('start_time') }}</div>-->
+          <!--<div v-if="errors.has('f-regular-start-time')" class="f-error">{{ errors.first('f-regular-start-time') }}</div>-->
           </tooltip>
         </div>
       </div>
