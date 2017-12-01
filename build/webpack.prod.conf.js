f414eb27a3cef0c453d0538856fe71fd81bad1cc
Михайло Голубовський <lamerumiata@gmail.com>
2017-12-01 17:54:49 +0200

    remove comments in js

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -78,6 +78,9 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // UglifyJs do not support ES6+, you can also use babel-minify for better treeshaking: https://github.com/babel/minify
     new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      },
       compress: {
         warnings: false
       },
