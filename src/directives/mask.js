ff9d1618be8850f791cc876e3670b32c159ffe33
Михайло Голубовський <lamerumiata@gmail.com>
2019-09-11 14:24:42 +0300

    refactor(directive): mask

--- src/directives/mask.js
+++ src/directives/mask.js
@@ -7,46 +7,48 @@ function event(name) {
   return evt
 }
 
+function input(ev) {
+  let el = ev.target
+  if (!ev.isTrusted) return // avoid infinite loop
+  /* other properties to try to diferentiate InputEvent of Event (custom)
+  InputEvent (native)
+    cancelable: false
+    isTrusted: true
+
+    composed: true
+    isComposing: false
+    which: 0
+
+  Event (custom)
+    cancelable: true
+    isTrusted: false
+  */
+  // by default, keep cursor at same position as before the mask
+  let position = el.selectionEnd
+  // save the character just inserted
+  let digit = el.value[position - 1]
+  el.value = masker(el.value, el.mask, true)
+  // if the digit was changed, increment position until find the digit again
+  while (
+    position < el.value.length &&
+    el.value.charAt(position - 1) !== digit
+  ) {
+    position++
+  }
+  if (el === document.activeElement) {
+    el.setSelectionRange(position, position)
+    clearTimeout(el.timeout)
+    el.timeout = setTimeout(function() {
+      el.setSelectionRange(position, position)
+    }, 0)
+  }
+  el.dispatchEvent(event('input'))
+}
+
 export default {
   bind(el, binding) {
-    let timeout
-
-    el.addEventListener('input', ev => {
-      if (!ev.isTrusted) return // avoid infinite loop
-      /* other properties to try to diferentiate InputEvent of Event (custom)
-      InputEvent (native)
-        cancelable: false
-        isTrusted: true
-
-        composed: true
-        isComposing: false
-        which: 0
-
-      Event (custom)
-        cancelable: true
-        isTrusted: false
-      */
-      // by default, keep cursor at same position as before the mask
-      let position = el.selectionEnd
-      // save the character just inserted
-      let digit = el.value[position - 1]
-      el.value = masker(el.value, binding.value, true)
-      // if the digit was changed, increment position until find the digit again
-      while (
-        position < el.value.length &&
-        el.value.charAt(position - 1) !== digit
-      ) {
-        position++
-      }
-      if (el === document.activeElement) {
-        el.setSelectionRange(position, position)
-        clearTimeout(timeout)
-        timeout = setTimeout(function() {
-          el.setSelectionRange(position, position)
-        }, 0)
-      }
-      el.dispatchEvent(event('input'))
-    })
+    el.mask = binding.value
+    el.addEventListener('input', input)
 
     el.dispatchEvent(event('input'))
   },
@@ -56,5 +58,8 @@ export default {
       el.value = newDisplay
       el.dispatchEvent(event('input'))
     }
+  },
+  unbind(el) {
+    el.removeEventListener('input', input)
   },
 }
