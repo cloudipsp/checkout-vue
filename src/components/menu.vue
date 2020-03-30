02669831be8f59f2d3541486b5c0934a10f2508b
Михайло Голубовський <lamerumiata@gmail.com>
2020-03-30 16:50:13 +0300

    refactor(component): menu

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -3,20 +3,16 @@
     <div v-t="'methods'" class="f-block-hr f-title f-hidden-mobile" />
     <div v-t="'methods_m'" class="f-block f-title3 f-visible-mobile" />
     <div class="f-block-hr">
-      <template v-for="item in list">
+      <template v-for="item in methods">
         <f-button-pay-wallet
           v-if="show(item)"
           :key="item"
           tab="wallets"
           position="bottom"
         >
-          <template v-slot:default="{ open, classButton }">
-            <div
-              v-t="item"
-              :class="className(item)"
-              @click="open"
-            ></div>
-            <div style="display: none" :class="classButton"></div>
+          <template #default="{ open, classButton }">
+            <div v-t="item" :class="className(item)" @click="open" />
+            <div style="display: none" :class="classButton" />
           </template>
         </f-button-pay-wallet>
         <div
@@ -32,11 +28,12 @@
 </template>
 
 <script>
+import { mapState } from '@/utils/store'
+
 export default {
   computed: {
-    list() {
-      return this.store.state.options.methods
-    },
+    ...mapState('options', ['methods']),
+    ...mapState('router', ['method']),
     show() {
       return function(item) {
         return item === 'wallets'
@@ -44,11 +41,7 @@ export default {
     },
     className() {
       return function(item) {
-        return [
-          'f-item',
-          'f-i-' + item,
-          { active: this.store.state.router.method === item },
-        ]
+        return ['f-item', 'f-i-' + item, { active: this.method === item }]
       }
     },
   },
