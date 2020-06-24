11d67c66b7c92d2b45b9c1f4555d0a0edaaaf720
Михайло Голубовський <lamerumiata@gmail.com>
2020-06-24 10:10:39 +0300

    refactor(mixin): resize

--- src/components/security.vue
+++ src/components/security.vue
@@ -1,39 +1,46 @@
 <template>
   <div class="f-security">
-    <span class="f-title-security" @click="open">
-      <f-svg ref="security" name="security" size="lg" />
-      <span
-        v-t="'security_title'"
-        @mouseenter="mouseenter"
-        @mouseleave="mouseleave"
-      />
-    </span>
-    <f-modal-base v-if="isMobile" v-model="showModal">
-      <div class="f-modal-security-title">
-        <f-svg name="security" size="lg" />
+    <template v-if="isMobile">
+      <span class="f-title-security" @click="open">
+        <f-svg ref="security" name="security" size="lg" />
         <span v-t="'security_title'" />
-      </div>
-      <!--eslint-disable-next-line vue/no-v-html-->
-      <div class="f-modal-security-content" v-html="$t('security_text')" />
-      <div class="f-modal-security-icons">
-        <svg-verified-by-visa />
-        <svg-master-card-secure-code />
-        <svg-pci-dss />
-      </div>
-    </f-modal-base>
-    <f-tooltip-default
-      v-else
-      :show.sync="showTooltip"
-      :target="() => $refs.security.$el"
-    >
-      <div class="f-tooltip-security-icons">
-        <svg-verified-by-visa />
-        <svg-master-card-secure-code />
-        <svg-pci-dss />
-      </div>
-      <!--eslint-disable-next-line vue/no-v-html-->
-      <div class="f-tooltip-security-content" v-html="$t('security_text')" />
-    </f-tooltip-default>
+      </span>
+      <f-modal-base v-model="showModal">
+        <div class="f-modal-security-title">
+          <f-svg name="security" size="lg" />
+          <span v-t="'security_title'" />
+        </div>
+        <!--eslint-disable-next-line vue/no-v-html-->
+        <div class="f-modal-security-content" v-html="$t('security_text')" />
+        <div class="f-modal-security-icons">
+          <svg-verified-by-visa />
+          <svg-master-card-secure-code />
+          <svg-pci-dss />
+        </div>
+      </f-modal-base>
+    </template>
+    <template v-else>
+      <span class="f-title-security">
+        <f-svg ref="security" name="security" size="lg" />
+        <span
+          v-t="'security_title'"
+          @mouseenter="mouseenter"
+          @mouseleave="mouseleave"
+        />
+      </span>
+      <f-tooltip-default
+        :show.sync="showTooltip"
+        :target="() => $refs.security.$el"
+      >
+        <div class="f-tooltip-security-icons">
+          <svg-verified-by-visa />
+          <svg-master-card-secure-code />
+          <svg-pci-dss />
+        </div>
+        <!--eslint-disable-next-line vue/no-v-html-->
+        <div class="f-tooltip-security-content" v-html="$t('security_text')" />
+      </f-tooltip-default>
+    </template>
   </div>
 </template>
 
@@ -46,7 +53,6 @@ export default {
     return {
       showTooltip: false,
       showModal: false,
-      isMobile: false,
     }
   },
   methods: {
@@ -58,11 +64,6 @@ export default {
     },
     open() {
       this.showModal = true
-    },
-    resize() {
-      let width = document.documentElement.clientWidth
-
-      this.isMobile = width < 576
     },
   },
 }
--- src/mixins/resize.js
+++ src/mixins/resize.js
@@ -1,12 +1,24 @@
 export default {
+  data() {
+    return {
+      isMobile: false,
+    }
+  },
   mounted: function() {
-    window.addEventListener('resize', this.resize)
-    this.resize()
+    window.addEventListener('resize', this.afterResize)
+    this.afterResize()
   },
   beforeDestroy: function() {
-    window.removeEventListener('resize', this.resize)
+    window.removeEventListener('resize', this.afterResize)
   },
   methods: {
+    afterResize() {
+      let width = document.documentElement.clientWidth
+
+      this.isMobile = width < 768
+
+      this.resize()
+    },
     resize() {},
   },
 }
