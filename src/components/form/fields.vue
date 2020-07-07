d4eaa4c19e604ef93fda6ac269a8feab1bb36482
Михайло Голубовський <lamerumiata@gmail.com>
2020-07-07 18:15:33 +0300

    chore(component): add form fields

---
+++ src/components/form/fields.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="f-form-fields">
+    <component
+      :is="field.component"
+      v-for="field in fields"
+      :key="field.name"
+      v-bind="field"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    fields: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+</script>
