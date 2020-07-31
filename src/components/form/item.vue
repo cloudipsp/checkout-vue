5e4d721a85eea38f62a72200dba93634c07faf25
Михайло Голубовський <lamerumiata@gmail.com>
2020-07-31 17:26:51 +0300

    chore(component): add form-item

---
+++ src/components/form/item.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="name" :class="className" v-bind="$attrs" v-on="$listeners">
+    <slot v-for="slot in Object.keys($slots)" :slot="slot" :name="slot" />
+  </component>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    component: {
+      type: String,
+      default: 'input',
+      validator: value => ['input', 'date'].includes(value),
+    },
+  },
+  computed: {
+    name() {
+      return 'f-form-item-' + this.component
+    },
+    className() {
+      return 'f-form-item-' + this.component
+    },
+  },
+}
+</script>
