ebafc08a3474b53fb274104d8f8f715d2cfab0dc
Михайло Голубовський <lamerumiata@gmail.com>
2020-07-31 17:26:32 +0300

    chore(component): add form-group

---
+++ src/components/form/group.vue
@@ -0,0 +1,111 @@
+<template>
+  <div :class="classGroup" @mouseenter="mouseenter" @mouseleave="mouseleave">
+    <f-form-item
+      v-bind="$attrs"
+      v-on="$listeners"
+      @error="onError"
+      @focus="focus"
+      @blur="blur"
+      @input="input"
+    />
+    <label v-if="label" :class="classLabel" :for="_id">
+      {{ $t(label) }}
+    </label>
+    <transition name="slide-fade">
+      <div v-if="showError" class="f-error">{{ error }}</div>
+    </transition>
+    <slot />
+  </div>
+</template>
+
+<script>
+import { mapState } from '@/utils/store'
+import { isExist } from '@/utils/typeof'
+
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default() {
+        return this.$attrs.name
+      },
+    },
+    labelClass: {
+      type: String,
+      default: 'f-control-label-p',
+    },
+    tooltip: {
+      type: Boolean,
+      default: false,
+    },
+    hideError: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      focused: false,
+      touched: false,
+      hover: false,
+      error: '',
+      value: null,
+    }
+  },
+  computed: {
+    ...mapState(['css', 'submit']),
+    _name() {
+      return 'f-' + this.$attrs.name
+    },
+    _id() {
+      return this.$attrs.id || this._name
+    },
+    classGroup() {
+      return ['f-form-group', this.hasError ? this.css.he : '']
+    },
+    classLabel() {
+      return [
+        this.css.cl,
+        this.labelClass,
+        this.hasError ? this.css.le : '',
+        {
+          'f-control-label-active':
+            (isExist(this.value) && this.value !== '') || this.focused,
+        },
+        { 'f-control-label-hover': this.hover },
+        { 'f-control-label-focused': this.focused },
+      ]
+    },
+    hasError() {
+      return this.error && (this.touched || this.submit)
+    },
+    showError() {
+      let showError = !this.tooltip && this.hasError && this.focused
+      this.$emit('show-error', showError, this.error)
+      return showError && !this.hideError
+    },
+  },
+  methods: {
+    onError(error) {
+      this.error = error
+    },
+    focus() {
+      this.focused = true
+    },
+    blur() {
+      this.focused = false
+      this.touched = true
+    },
+    input(value) {
+      this.value = value
+    },
+    mouseenter() {
+      this.hover = true
+    },
+    mouseleave() {
+      this.hover = false
+    },
+  },
+}
+</script>
