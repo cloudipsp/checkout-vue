d199c896ee1526507ce75a1d86ab4e5e841996ce
Михайло Голубовський <lamerumiata@gmail.com>
2020-08-14 14:23:04 +0300

    refactor(component): input-checkbox, add props variant

--- src/components/input-checkbox.vue
+++ src/components/input-checkbox.vue
@@ -7,11 +7,11 @@
       :data-vv-as="label_"
       :data-vv-name="name_"
       type="checkbox"
-      class="f-checkbox"
+      :class="className"
       @focus="focus"
       @blur="blur"
     />
-    <label :class="[css.cl]" :for="name_"><slot /></label>
+    <label class="f-checkbox-label" :for="name_"><slot /></label>
     <transition name="slide-fade">
       <div v-if="showError" class="f-error">
         {{ errors.first(name_) }}
@@ -25,5 +25,23 @@ import Input from '@/mixins/input'
 
 export default {
   mixins: [Input],
+  props: {
+    variant: {
+      type: String,
+      default: 'default',
+      validator: value => ['default', 'secondary'].includes(value),
+    },
+  },
+  computed: {
+    className() {
+      return [
+        'f-checkbox',
+        `f-checkbox-${this.variant}`,
+        this.classReadonly,
+        this.classError,
+        this.inputClass,
+      ]
+    },
+  },
 }
 </script>
