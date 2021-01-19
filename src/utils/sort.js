712dc5e8bd0cc3ddf380eb8bf993283144412fe0
Михайло Голубовський <lamerumiata@gmail.com>
2021-01-19 14:32:33 +0200

    refactor(utils):sort

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,9 +1,10 @@
 import i18n from '@/i18n'
+import { isString } from '@/utils/typeof'
 
 export const sort = (field, reverse) => {
   reverse = reverse ? -1 : 1
   return (a, b) => {
-    if (String.prototype.localeCompare) {
+    if (isString(a[field]) && String.prototype.localeCompare) {
       return a[field].localeCompare(b[field], i18n.locale) * reverse
     } else {
       return (a[field] < b[field] ? -1 : 1) * reverse
