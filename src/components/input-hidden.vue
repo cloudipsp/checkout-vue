28c007e5ae2ac873bb197cf73df263f4af725665
Михайло Голубовський <lamerumiata@gmail.com>
2021-01-27 15:13:41 +0200

    refactor(component): input-hidden

--- src/components/input-hidden.vue
+++ src/components/input-hidden.vue
@@ -1,8 +1,28 @@
 <script>
-import Input from '@/mixins/input'
+import { mapState } from '@/utils/store'
 
 export default {
-  mixins: [Input],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+    custom: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    ...mapState(['params']),
+  },
+  created() {
+    let form = this.custom ? this.params.custom : this.params
+    form[this.name] = form[this.name] || this.value
+  },
   render(h) {
     return h()
   },
