bd70717f20596431222301dcb9ff41bb8502583b
Михайло Голубовський <lamerumiata@gmail.com>
2021-02-16 20:34:32 +0200

    refactor: compress svg

--- src/svg/approved.vue
+++ src/svg/approved.vue
@@ -1,64 +1,45 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    style="isolation: isolate"
-    viewBox="0 0 130 121"
-    width="130px"
-    height="121px"
-  >
-    <defs>
-      <clipPath id="_clipPath_1A1Vmg5OcuT1LfsrWtwYLRy05dDSTX72">
-        <rect width="130" height="121" />
-      </clipPath>
-    </defs>
-    <g clip-path="url(#_clipPath_1A1Vmg5OcuT1LfsrWtwYLRy05dDSTX72)">
-      <g>
-        <g filter="url(#TxoI3LRnnEomP5t74sCuQFPQnrG1rr3I)">
-          <ellipse
-            vector-effect="non-scaling-stroke"
-            cx="64.99999999999997"
-            cy="60.50000000000002"
-            rx="32.999999999999986"
-            ry="28.500000000000007"
-            fill="rgb(213,218,224)"
-          />
-        </g>
-        <defs>
-          <filter
-            id="TxoI3LRnnEomP5t74sCuQFPQnrG1rr3I"
-            x="-200%"
-            y="-200%"
-            width="400%"
-            height="400%"
-            filterUnits="objectBoundingBox"
-            color-interpolation-filters="sRGB"
-          >
-            <feGaussianBlur
-              xmlns="http://www.w3.org/2000/svg"
-              stdDeviation="16"
-            />
-          </filter>
-        </defs>
-        <path
-          d=" M 34.5 40 C 34.5 37.239 36.739 35 39.5 35 L 85.5 35 C 88.261 35 90.5 37.239 90.5 40 L 90.5 43 L 34.5 43 L 34.5 40 Z  M 34.5 49 L 90.5 49 L 90.5 66 C 90.5 68.761 88.261 71 85.5 71 L 39.5 71 C 36.739 71 34.5 68.761 34.5 66 L 34.5 49 Z  M 38.5 54 L 50.5 54 L 50.5 57 L 38.5 57 L 38.5 54 Z  M 53.5 54 L 57.5 54 L 57.5 57 L 53.5 57 L 53.5 54 Z "
-          fill-rule="evenodd"
-          fill="rgb(255,255,255)"
-        />
-        <path
-          d=" M 93.667 72.888 C 93.667 78.779 88.891 83.555 83 83.555 C 77.109 83.555 72.334 78.779 72.334 72.888 C 72.334 66.997 77.109 62.222 83 62.222 C 88.891 62.222 93.667 66.997 93.667 72.888 Z "
-          fill="rgb(48,215,105)"
-        />
-        <path
-          d=" M 90.368 69.805 C 90.875 69.27 90.852 68.426 90.317 67.92 C 89.783 67.413 88.939 67.436 88.432 67.971 L 81.821 74.949 L 78.368 71.304 C 77.862 70.77 77.018 70.747 76.483 71.253 C 75.949 71.76 75.926 72.604 76.432 73.138 L 80.853 77.805 C 81.105 78.071 81.455 78.221 81.821 78.221 C 82.187 78.221 82.537 78.071 82.789 77.805 L 90.368 69.805 Z "
-          fill="rgb(255,255,255)"
-        />
-        <path
-          d=" M 82.999 85.554 C 89.995 85.554 95.666 79.883 95.666 72.888 C 95.666 65.892 89.995 60.221 82.999 60.221 C 76.004 60.221 70.333 65.892 70.333 72.888 C 70.333 79.883 76.004 85.554 82.999 85.554 Z  M 93.666 72.888 C 93.666 78.779 88.89 83.554 82.999 83.554 C 77.108 83.554 72.333 78.779 72.333 72.888 C 72.333 66.997 77.108 62.221 82.999 62.221 C 88.89 62.221 93.666 66.997 93.666 72.888 Z "
-          fill-rule="evenodd"
-          fill="rgb(255,255,255)"
-        />
-      </g>
+  <svg xmlns="http://www.w3.org/2000/svg" width="130" height="121">
+    <g filter="url(#a)">
+      <ellipse
+        vector-effect="non-scaling-stroke"
+        cx="65"
+        cy="60.5"
+        rx="33"
+        ry="28.5"
+        fill="#D5DAE0"
+      />
     </g>
+    <defs>
+      <filter
+        id="a"
+        x="-200%"
+        y="-200%"
+        width="400%"
+        height="400%"
+        filterUnits="objectBoundingBox"
+        color-interpolation-filters="sRGB"
+      >
+        <feGaussianBlur xmlns="http://www.w3.org/2000/svg" stdDeviation="16" />
+      </filter>
+    </defs>
+    <path
+      d="M34.5 40a5 5 0 015-5h46a5 5 0 015 5v3h-56v-3zm0 9h56v17a5 5 0 01-5 5h-46a5 5 0 01-5-5V49zm4 5h12v3h-12v-3zm15 0h4v3h-4v-3z"
+      fill-rule="evenodd"
+      fill="#FFF"
+    />
+    <path
+      d="M93.667 72.888c0 5.891-4.776 10.667-10.667 10.667s-10.666-4.776-10.666-10.667S77.109 62.222 83 62.222s10.667 4.775 10.667 10.666z"
+      fill="#30D769"
+    />
+    <path
+      d="M90.368 69.805a1.333 1.333 0 10-1.936-1.834l-6.611 6.978-3.453-3.645a1.334 1.334 0 00-1.936 1.834l4.421 4.667a1.333 1.333 0 001.936 0l7.579-8z"
+      fill="#FFF"
+    />
+    <path
+      d="M82.999 85.554c6.996 0 12.667-5.671 12.667-12.666 0-6.996-5.671-12.667-12.667-12.667-6.995 0-12.666 5.671-12.666 12.667 0 6.995 5.671 12.666 12.666 12.666zm10.667-12.666c0 5.891-4.776 10.666-10.667 10.666s-10.666-4.775-10.666-10.666 4.775-10.667 10.666-10.667 10.667 4.776 10.667 10.667z"
+      fill-rule="evenodd"
+      fill="#FFF"
+    />
   </svg>
 </template>
