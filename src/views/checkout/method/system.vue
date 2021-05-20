49c03f7c5820d5cccc2d6214af336951c08ba631
Михайло Голубовський <lamerumiata@gmail.com>
2021-05-20 13:27:57 +0300

    feat(component): system; add fields email and customer (WAL-342)

--- src/views/checkout/method/system.vue
+++ src/views/checkout/method/system.vue
@@ -10,6 +10,8 @@
     <div class="f-bank-desc" v-text="$t('bank_desc')" />
     <div class="f-container-sm">
       <f-fields-bank :fields="form.fields" />
+      <f-field-email />
+      <f-fields-customer />
       <f-fields-button />
       <f-fields-user />
       <f-offer />
@@ -22,6 +24,8 @@
 import FIcon from '@/components/icon'
 import { FButtonClose } from '@/components/button/button-close'
 import FFieldsBank from '@/components/fields/bank'
+import FFieldEmail from '@/components/fields/email'
+import FFieldsCustomer from '@/components/fields/customer'
 import FFieldsButton from '@/components/fields/button'
 import FFieldsUser from '@/components/fields/user'
 import FOffer from '@/components/offer'
@@ -33,6 +37,8 @@ export default {
     FIcon,
     FButtonClose,
     FFieldsBank,
+    FFieldEmail,
+    FFieldsCustomer,
     FFieldsButton,
     FFieldsUser,
     FOffer,
