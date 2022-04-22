5abb559b73fbae9bfd7bebaa7ecb39b3c7555bce
Михайло Голубовський <lamerumiata@gmail.com>
2022-04-22 18:34:34 +0300

    fix(form-select): double click on arrow

--- src/components/form/item/select.vue
+++ src/components/form/item/select.vue
@@ -12,19 +12,16 @@
       v-on="$listeners"
       @keyup.enter="onEnter"
     />
-    <f-svg name="angle-down" />
   </ValidationProvider>
 </template>
 
 <script>
 import { FFormSelect } from '@/components/form/item/helpers/form-select'
-import FSvg from '@/components/svg'
 import { itemMixin } from '@/mixins/item'
 
 export default {
   components: {
     FFormSelect,
-    FSvg,
   },
   mixins: [itemMixin],
 }
