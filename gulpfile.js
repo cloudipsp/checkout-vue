7e52fdb95e305b7d0fec09ecea309c79d2adad1a
Михайло Голубовський <lamerumiata@gmail.com>
2022-08-08 12:03:12 +0300

    build: сreating list for search countries (WAL-433)

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const fsp = require('fs').promises
 const gulp = require('gulp')
 const nodeGettextGenerator = require('node-gettext-generator')
 const configLocale = require('./src/config/locales.json')
@@ -50,7 +51,8 @@ gulp.task('po', function (done) {
         // placeholder: 'tran4'
         // name: 'tran4'
         // { path: 'tran4'
-        match: /(?:(?<!(?:(?:svg|transition|slot)[ ])|[^ ])(?:title|text|name|label|placeholder)=(["'])([^{}]+?)\1)|(?:v-t="'(.+?)'")|(?:\$t\((["'])(.+?)\4)|(?:(?:label|placeholder|name|{ path):[ ]?(["'])(.+?)\6)/g,
+        match:
+          /(?:(?<!(?:(?:svg|transition|slot)[ ])|[^ ])(?:title|text|name|label|placeholder)=(["'])([^{}]+?)\1)|(?:v-t="'(.+?)'")|(?:\$t\((["'])(.+?)\4)|(?:(?:label|placeholder|name|{ path):[ ]?(["'])(.+?)\6)/g,
         replace: "_('$2$3$5$7')",
       },
       params: {
@@ -67,3 +69,55 @@ gulp.task('po', function (done) {
 })
 
 gulp.task('locale', gulp.series('methods', 'subscription-period', 'po'))
+
+gulp.task('countries', function (done) {
+  fsp
+    .readFile('./node_modules/countries-languages/lib/countries.json', 'utf-8')
+    .then(content => {
+      let obj = JSON.parse(content)
+      let result = Object.entries(obj).reduce(
+        (
+          acum,
+          [
+            key,
+            {
+              name,
+              translations,
+              alt_spellings,
+              iso_3166_1_alpha2,
+              iso_3166_1_alpha3,
+            },
+          ]
+        ) => {
+          let common = Object.values({
+            ...name.native,
+            ...translations,
+          }).map(({ common }) => common)
+
+          common = [
+            ...common,
+            iso_3166_1_alpha2,
+            iso_3166_1_alpha3,
+            name.common,
+            ...(alt_spellings || []),
+          ]
+            .filter((item, key, self) => self.indexOf(item) === key)
+            .map(item => item.toLowerCase())
+
+          acum[key] = common
+          return acum
+        },
+        {}
+      )
+
+      return fsp.writeFile(
+        './src/config/countries-search.js',
+        `export const countriesSearch = ${JSON.stringify(result, null, 2)}`
+      )
+    })
+    .then(function () {
+      done()
+    })
+})
+
+gulp.task('default', gulp.series(['locale', 'countries']))
