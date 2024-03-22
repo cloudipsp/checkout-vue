34fc1f0fa8ab18ca3a9102bdbe3af27ebdcf359e
Михайло Голубовський <lamerumiata@gmail.com>
2024-03-22 19:53:11 +0200

    build(gulp): adding task i18n-need-translation

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,50 @@ gulp.task('po', done =>
     })
 )
 
-gulp.task('locale', gulp.series('methods', 'subscription-period', 'po'))
+gulp.task('i18n', gulp.series('methods', 'subscription-period', 'po'))
+
+gulp.task(
+  'i18n-need-translation',
+  gulp.series('i18n', () => {
+    const from = 'en'
+    const dirname = `./src/i18n/need-translation/`
+    let translation
+
+    return fsp
+      .readFile(`./src/i18n/po/${from}/translation.json`, 'utf-8')
+      .then(content => JSON.parse(content))
+      .then(content => (translation = content))
+      .then(() => fsp.access(dirname).catch(() => fsp.mkdir(dirname)))
+      .then(() =>
+        fsp
+          .readdir(dirname)
+          .then(filenames =>
+            filenames.map(filename => fsp.rm(dirname + filename))
+          )
+          .then(promises => Promise.all(promises))
+      )
+      .then(() =>
+        locales
+          .filter(locale => locale !== from)
+          .map(locale =>
+            fsp
+              .readFile(`./src/i18n/po/${locale}/translation.json`, 'utf-8')
+              .then(content => JSON.parse(content))
+              .then(content => Object.entries(content))
+              .then(content => content.filter(([, value]) => !value))
+              .then(content => content.map(([name]) => name))
+              .then(content =>
+                content.map(
+                  name => `msgid "${name}"\nmsgstr "${translation[name]}"\n`
+                )
+              )
+              .then(content => content.join('\n'))
+              .then(content => fsp.writeFile(`${dirname}${locale}.po`, content))
+          )
+      )
+      .then(promises => Promise.all(promises))
+  })
+)
 
 gulp.task('countries-search', () =>
   fsp
@@ -198,7 +241,7 @@ gulp.task('svg', () => {
 gulp.task(
   'default',
   gulp.series([
-    'locale',
+    'i18n',
     'countries-search',
     'countries-calling-codes',
     'exclude-message',
