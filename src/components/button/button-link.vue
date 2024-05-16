b401181d584601c2247122f835ff94b3142e6ec5
Михайло Голубовський <lamerumiata@gmail.com>
2024-05-16 14:48:37 +0300

    refactor: btn-link

--- src/components/button/button-link.vue
+++ src/components/button/button-link.vue
@@ -15,16 +15,99 @@ export default {
     variant: makeProp(PROP_TYPE_STRING, 'default', value =>
       arrayIncludes(['default', 'secondary'], value)
     ),
+    size: makeProp(PROP_TYPE_STRING, undefined, value =>
+      arrayIncludes(['56'], value)
+    ),
   },
   computed: {
     className() {
       return [
-        'f-btn',
-        'f-btn-link',
-        `f-btn-link-${this.variant}`,
-        { 'f-btn-block': this.block },
+        this.$style.style,
+        this.$style[this.variant],
+        this.$style[`s_${this.size}`],
+        {
+          [this.$style.block]: this.block,
+        },
       ]
     },
   },
 }
 </script>
+
+<style lang="scss" module>
+@mixin button-link-variant($color, $color-hover, $color-active) {
+  color: $color;
+
+  &:hover,
+  &:focus {
+    color: $color-hover;
+  }
+
+  &:active {
+    color: $color-active;
+  }
+}
+
+.style {
+  position: relative;
+  display: inline-block;
+  height: px-to-rem(44px);
+  min-height: px-to-rem(44px);
+  padding: px-to-rem(10px) px-to-rem(32px);
+  font-size: px-to-rem(16px);
+  font-weight: 500;
+  line-height: px-to-rem(24px);
+  text-align: center;
+  background: none;
+  border: none;
+  border-radius: px-to-rem(8px);
+  transition: all ease-in-out 0.15s;
+  white-space: nowrap;
+
+  &:hover,
+  &:focus {
+    text-decoration: none;
+    transform: translateY(px-to-rem(-2px));
+  }
+
+  &:focus-visible {
+    outline: 0;
+    box-shadow: 0 0 0 px-to-rem(2px) $outline_border;
+  }
+
+  &:active {
+    transform: translateY(0);
+  }
+
+  :not(:disabled, .disabled) {
+    cursor: pointer;
+  }
+}
+
+.default {
+  @include button-link-variant(
+    $btn_link_default_color,
+    $btn_link_default_hover_color,
+    $btn_link_default_active_color
+  );
+}
+
+.secondary {
+  @include button-link-variant(
+    $btn_link_secondary_color,
+    $btn_link_secondary_hover_color,
+    $btn_link_secondary_active_color
+  );
+}
+
+.s_56 {
+  height: px-to-rem(56px);
+  min-height: px-to-rem(56px);
+  padding: px-to-rem(16px) px-to-rem(32px);
+}
+
+.block {
+  display: block;
+  width: 100%;
+}
+</style>
